12a266d5a3398fc4f2ed46110e41c7d2eff6e1f3
Hampus Avekvist <hampus@avekvist.com>
2019-04-01 00:39:01 +0200

    Commented the code. Updated the design a bit.

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -1,6 +1,11 @@
+// Initializing the height of the nav
+// as well as the reference to it. 
 var navHeight = 0;
 var nav = 0;
 
+// The entire HTML for the navbar.
+// If JavaScript is deactivated, it shouldn't
+// exist. 
 var navString = `
 		<nav id="outer-nav">
 			<div id="nav-handle">
@@ -19,29 +24,43 @@ var navString = `
 			<a id="nav-performant-button" class="nav-button">performant</a>
 			<a id="nav-expressive-button" class="nav-button">expressive</a>
 			<a id="nav-about-button" class="nav-button">about</a>
-			<a id="nav-links-button" class="nav-button">links</a>
 			<a id="nav-bottom-button" class="nav-button">copyright</a>
 		</nav>`;
 
+// Function to initialize (and reinitialize) the navbar. 
 var initialize = function() {
+	// Adds a new nav if it doesn't exist. 
 	if(nav == 0)
 		nav = $("body").prepend(navString);
-	
+
+	// Sets the display mode on the handle to initial. 
 	$("#nav-handle").css("display", "initial");
 	
-	if($(window).width() < 640) {
-		$("#inner-nav").css("top", $("#outer-nav").height());
+	// Sets the top position of the navbar.  
+	$("#inner-nav").css("top", $("#outer-nav").height());
+	
+	// Checks the window width and initializes the 
+	// inner navbar correctly. Only for larger
+	// screebs
+	if($(window).width() > 640) {
 		$("#inner-nav").css("right", $(document).width() / 2 - $("#inner-nav").width() / 2);
 	}
 
+	// Sets the height of the navbar. 
 	navHeight = $("#outer-nav").height();
 }
 
+// Function that toggles the navbar. 
 var toggleNavBar = function() {
+	// Depending on the size of the window
+	// or the size of the device used, 
+	// the height of the navbar changes. 
 	var height = "60vh";
 	if($(window).width() < 640)
 		height = "150vh";
 	
+	// Depending on the height of the navbar,
+	// it sets the height and animation. 
 	if($("#inner-nav").css("height") == "0px") {
 		$("#inner-nav").animate({
 			"height": height,
@@ -52,78 +71,103 @@ var toggleNavBar = function() {
 		}, 300);
 	}
 
+	// An attempt to toggle the pieces of bread 
+	// at the top of the screen. 
 	$("#nav-button-bar1").toggleClass("active-nav");
 	$("#nav-button-bar2").toggleClass("active-nav");
 	$("#nav-button-bar3").toggleClass("active-nav");
 }
 
+// Function to scroll to a specific position
+// For smooth scrolling. 
 var scrollTo = function(ypos) {
 	$("html, body").animate({
 		scrollTop: ypos,
 	}, 1000);
 }
 
-/*
-var scrollToTag = function(accessor) {
-	scrollTo($(accessor).offset().top);
-}
-*/
-
+// A small helper function, an abstraction
+// that makes it easier to smooth scroll
+// to sections. It's also subtracting
+// the height of the navbar handle. 
 var scrollToSection = function(accessor) {
 	scrollTo($(accessor).offset().top - navHeight);
 }
 
+// When the document is ready, this runs. 
 $(document).ready(function() {
 	initialize();
 	
+	// Binds the on-click callback for the 
+	// #nav-handle. 
 	$("#nav-handle").on('click', function() {
 		toggleNavBar();
 	});
 	
+	// Binds the on-click callback for the
+	// .nav-button. 
 	$(".nav-button").on('click', function() {
 		toggleNavBar();
 	});
 	
+	// Binds the resize callback to the
+	// initialize function.
+	// This code runs when the window
+	// is resized. 
 	$(window).resize(function() {
 		initialize();
 	});
 	
+	// Clicking the #precise-button scrolls
+	// you to the correct section. 
 	$("#precise-button").on('click', function() {
 		scrollToSection("#precise");
 	});
 	
+	// Clicking the #performant-button scrolls
+	// you to the correct section. 
 	$("#performant-button").on('click', function() {
 		scrollToSection("#performant");
 	});
 	
+	// Clicking the #expressive-button scrolls
+	// you to the correct section. 
 	$("#expressive-button").on('click', function() {
 		scrollToSection("#expressive");
 	});
 	
+	// Clicking the #nav-top-button scrolls
+	// you to the top. 
 	$("#nav-top-button").on('click', function() {
 		scrollTo(0);
 	});
 	
+	// Clicking the #nav-precise-button scrolls
+	// you to the correct section. 
 	$("#nav-precise-button").on('click', function() {
 		scrollToSection("#precise");
 	});
 	
+	// Clicking the #nav-performant-button scrolls
+	// you to the correct section. 
 	$("#nav-performant-button").on('click', function() {
 		scrollToSection("#performant");
 	});
 	
+	// Clicking the #nav-expressive-button scrolls
+	// you to the correct section. 
 	$("#nav-expressive-button").on('click', function() {
 		scrollToSection("#expressive");
 	});
 	
+	// Clicking the #nav-about-button scrolls
+	// you to the correct section. 
 	$("#nav-about-button").on('click', function() {
 		scrollToSection("#about");
 	});
-	
-	$("#nav-links-button").on('click', function() {
-		scrollToSection("#links");
-	});
-	
+
+	// Clicking the #nav-bottom-button scrolls
+	// you to the bottom of the document. 
 	$("#nav-bottom-button").on('click', function() {
 		scrollTo($(document).height() - $(window).height());
 	});
--- docs/js/validate_form.js
+++ docs/js/validate_form.js
@@ -1,15 +1,32 @@
+// Execute this code when the entire site
+// has finished loading. 
 $(document).ready(function() {
+    // Initialize the regular expression. 
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    // When you type in the form, this function
+    // is called
     $("#email-form").keyup(function() {
+        // If the length of the contents in the form
+        // is greater than 0 then
         if($("#email-form").val().length > 0) {
+
+            // check if the content matches the regular
+            // expression rules specified above. 
             var correct = re.exec($("#email-form").val());
+
+            // If not, then
             if(!correct) {
-                $("#validator-text").text("Enter a correct email address");
+                // disable the submit button. 
+                $("#email-form-submit").prop("disabled", true);
             } else {
-                $("#validator-text").text("Awesome, just hit 'Join' and start receiving emails. ");
+                // Otherwise, enable it. 
+                $("#email-form-submit").prop("disabled", false);
             }
         } else {
-            $("#validator-text").text("");
+            // If the content length isn't greater than 0
+            // then disable the button. 
+            $("#email-form-submit").prop("disabled", true);
         }
     })
 });
