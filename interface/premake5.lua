dfec08eb3a9188773b22ae27d8bb6747484435f4
Hampus Avekvist <hampus@avekvist.com>
2019-04-14 22:03:31 +0200

    added premake

---
+++ interface/premake5.lua
@@ -0,0 +1,20 @@
+project "equality"
+    kind "StaticLib"
+    language "C"
+    systemversion "latest"
+    staticruntime "on"
+
+    targetdir ("binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("intermediates" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "/source/**.h",
+        "/source/**.c"
+    }
+
+    includedirs {
+        "/source/include"
+    }
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
